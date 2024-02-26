13e4edfb95e92495619fd732690365cc42068727
Thiago Pacheco <thiagovpacheco@gmail.com>
2024-02-26 13:49:34 -0300

    propriedades de componentes dinâmicos

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  
+  @Input()
+  photoCover:string = "https://t.ctcdn.com.br/9KZ0WLOfJFYa4ZYBsjQ82Gpa6dM=/640x360/smart/i821523.png";
+  @Input()
+  cardTitle:string = "Saiu a nova versão do Angular"
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
